558f11e72d43c313a1995759fd49e415b4fd43ae
Beyaz Zambak <v1kusen04ek@mail.ru>
2023-06-06 19:24:07 +0300

    Task 2

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import sqlalchemy
+import json
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import or_
+from models import create_tables, Publisher, Book, Sale, Stock, Shop
+
 
 DNS = 'postgresql://postgres:12345@localhost:5432/sqlalchemy_db'
 engine = sqlalchemy.create_engine(DNS)
@@ -7,9 +11,43 @@ engine = sqlalchemy.create_engine(DNS)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+create_tables(engine)
 
+def filling_test_data():
+    with open('test_data.json') as f:
+        json_data = json.load(f)
 
+    for data in json_data:
+        if data['model'] == 'publisher':
+            a = Publisher(id = data['pk'], name = data['fields']['name'])
+        elif data['model'] == 'book':
+            a = Book(id = data['pk'], title = data['fields']['title'], id_publisher = data['fields']['id_publisher'])
+        elif data['model'] == 'shop':
+            a = Shop(id = data['pk'], name = data['fields']['name'])
+        elif data['model'] == 'stock':
+            a = Stock(id = data['pk'], id_shop = data['fields']['id_shop'], id_book = data['fields']['id_book'], count = data['fields']['count'])
+        elif data['model'] == 'sale':
+            a = Sale(id = data['pk'], price = data['fields']['price'], date_sale = data['fields']['date_sale'], count = data['fields']['count'], id_stock = data['fields']['id_stock'])
+        session.add(a)
+        session.commit()
+filling_test_data()
 
+def get_publisher_shops(publisher):
+    query = session.query(Shop)
+    query = query.join(Stock)
+    query = query.join(Book)
+    query = query.join(Publisher)
+    if publisher.isdigit():
+        query = query.filter(Publisher.id == publisher).all()
+    else:
+        query = query.filter(Publisher.name == publisher).all()
+    if query:
+        for record in query:
+            print(record)
+    else:
+        print(f'Издатель ({publisher}) не найден')
+
+get_publisher_shops(input(f'Введите имя или id издателя: '))
 
 
 session.close()
--- models.py
+++ models.py
@@ -7,16 +7,22 @@ class Publisher(Base):
     __tablename__ = 'publisher'
 
     id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.Text(length=60), nullable=False)
+    name = sq.Column(sq.String(length=60), nullable=False)
+
+    def __str__(self):
+        return f'Publisher {self.id}: {self.name}'
 
 class Book(Base):
     __tablename__ = 'book'
 
     id = sq.Column(sq.Integer, primary_key=True)
-    title = sq.Column(sq.Text(length=60), nullable=False)
+    title = sq.Column(sq.String(length=60), nullable=False)
     id_publisher = sq.Column(sq.Integer, sq.ForeignKey('publisher.id'), nullable=False)
     
     publisher = relationship(Publisher, backref="books")
+
+    def __str__(self):
+        return f'Book {self.id}: {self.title}, {self.id_publisher}'
 
 class Stock(Base):
     __tablename__ = 'stock'
@@ -28,24 +34,33 @@ class Stock(Base):
 
     book = relationship(Book, backref="stocks")
 
+    def __str__(self):
+        return f'Stock {self.id}: {self.id_book}, {self.id_shop}, {self.count}'
+
 class Sale(Base):
     __tablename__ = 'sale'
 
     id = sq.Column(sq.Integer, primary_key=True)
-    price = sq.Column(sq.Integer, nullable=False)
+    price = sq.Column(sq.Numeric, nullable=False)
     date_sale = sq.Column(sq.Date, nullable=False)
     id_stock = sq.Column(sq.Integer, sq.ForeignKey('stock.id'), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
 
     stock = relationship(Stock, backref="sales")
 
+    def __str__(self):
+        return f'Sale {self.id}: {self.price}, {self.date_sale}, {self.id_stock}, {self.count}'
+
 class Shop(Base):
     __tablename__ = 'shop'
 
     id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.Text(length=60), nullable=False)
+    name = sq.Column(sq.String(length=60), nullable=False)
 
     stock = relationship(Stock, backref="shop")
+
+    def __str__(self):
+        return f'Shop {self.id}: {self.name}'
 
 def create_tables(engine):
     Base.metadata.drop_all(engine)
